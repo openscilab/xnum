ef7939f2c1fe2a39cb9a1f76f6f0b523614e013e
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-07-10 19:41:33 +0330

    feat : params.py updated

--- xnum/params.py
+++ xnum/params.py
@@ -1,4 +1,32 @@
 # -*- coding: utf-8 -*-
 """XNum parameters and constants."""
+from enum import Enum
 
 XNUM_VERSION = "0.1"
+
+ENGLISH_DIGITS = "0123456789"
+PERSIAN_DIGITS = "۰۱۲۳۴۵۶۷۸۹"
+HINDI_DIGITS   = "०१२३४५६७८९"
+ARABIC_INDIC_DIGITS = "٠١٢٣٤٥٦٧٨٩"
+BENGALI_DIGITS = "০১২৩৪৫৬৭৮৯"
+
+NUMERAL_MAPS = {
+    "english": ENGLISH_DIGITS,
+    "persian": PERSIAN_DIGITS,
+    "hindi": HINDI_DIGITS,
+    "arabic_indic": ARABIC_INDIC_DIGITS,
+    "bengali": BENGALI_DIGITS
+}
+
+ALL_DIGIT_MAPS = {}
+for system, digits in NUMERAL_MAPS.items():
+    for idx, ch in enumerate(digits):
+        ALL_DIGIT_MAPS[ch] = str(idx)
+
+class NumeralSystem(Enum):
+    ENGLISH = "english"
+    PERSIAN = "persian"
+    HINDI = "hindi"
+    ARABIC_INDIC = "arabic_indic"
+    BENGALI = "bengali"
+    AUTO = "auto"
