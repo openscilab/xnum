58e68f3dc7253e8edadef3771f96ab8ddffa7405
Sepand Haghighi <sepand.haghighi@yahoo.com>
2025-07-15 18:11:48 +0330

    Base (#1)
    * feat : params.py updated
    
    * feat : functions.py updated
    
    * fix : __init__.py updated
    
    * fix : error messages moved to params.py
    
    * doc : docstrings updated
    
    * fix : minor edit in params.py variables
    
    * fix : autopep8
    
    * fix : __init__.py updated
    
    * doc : convert_match docstring updated
    
    * fix : autopep8
    
    * doc : CHANGELOG.md updated

--- xnum/params.py
+++ xnum/params.py
@@ -1,4 +1,38 @@
 # -*- coding: utf-8 -*-
 """XNum parameters and constants."""
+from enum import Enum
 
 XNUM_VERSION = "0.1"
+
+ENGLISH_DIGITS = "0123456789"
+PERSIAN_DIGITS = "۰۱۲۳۴۵۶۷۸۹"
+HINDI_DIGITS = "०१२३४५६७८९"
+ARABIC_INDIC_DIGITS = "٠١٢٣٤٥٦٧٨٩"
+BENGALI_DIGITS = "০১২৩৪৫৬৭৮৯"
+
+NUMERAL_MAPS = {
+    "english": ENGLISH_DIGITS,
+    "persian": PERSIAN_DIGITS,
+    "hindi": HINDI_DIGITS,
+    "arabic_indic": ARABIC_INDIC_DIGITS,
+    "bengali": BENGALI_DIGITS
+}
+
+ALL_DIGIT_MAPS = {}
+for system, digits in NUMERAL_MAPS.items():
+    for index, char in enumerate(digits):
+        ALL_DIGIT_MAPS[char] = str(index)
+
+
+class NumeralSystem(Enum):
+    ENGLISH = "english"
+    PERSIAN = "persian"
+    HINDI = "hindi"
+    ARABIC_INDIC = "arabic_indic"
+    BENGALI = "bengali"
+    AUTO = "auto"
+
+
+INVALID_SOURCE_MESSAGE = "Invalid value. `source` must be an instance of NumeralSystem enum."
+INVALID_TARGET_MESSAGE = "Invalid value. `target` must be an instance of NumeralSystem enum."
+INVALID_TEXT_MESSAGE = "Invalid value. `text` must be a string."
