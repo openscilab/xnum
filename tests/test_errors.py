5692a6b14b112c46953478c0634a0c4c043ee339
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-07-22 16:29:43 +0330

    fix : test_errors.py updated

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -9,9 +9,14 @@ def test_text_error():
         _ = convert(2, target=NumeralSystem.PERSIAN, source=NumeralSystem.ENGLISH)
 
 
-def test_target_error():
+def test_target_error1():
     with pytest.raises(ValueError, match=r"Invalid value. `target` must be an instance of NumeralSystem enum."):
         _ = convert("۱۲۳۴۵", target="English", source=NumeralSystem.PERSIAN)
+
+
+def test_target_error2():
+    with pytest.raises(ValueError, match=r"Invalid value. `target` cannot be NumeralSystem.AUTO."):
+        _ = convert("۱۲۳۴۵", target=NumeralSystem.AUTO, source=NumeralSystem.PERSIAN)
 
 
 def test_source_error():
