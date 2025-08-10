c333a0b55a092e1ffe78dec3c020bd048c996f04
AHReccese <amirhssin6rst@gmail.com>
2025-08-10 13:15:37 -0400

    replace `K` with `k`

--- tests/test_kannada.py
+++ tests/test_kannada.py
@@ -7,109 +7,109 @@ def test_identity_conversion():
     assert convert("೦೧೨೩೪೫೬೭೮೯", source=NumeralSystem.KANNADA, target=NumeralSystem.KANNADA) == "೦೧೨೩೪೫೬೭೮೯"
 
 
-def test_Kannada_to_english1():
+def test_kannada_to_english1():
     assert convert("೦೧೨೩೪೫೬೭೮೯", source=NumeralSystem.KANNADA, target=NumeralSystem.ENGLISH) == "0123456789"
 
 
-def test_Kannada_to_english2():
+def test_kannada_to_english2():
     assert convert("abc ೦೧೨೩೪೫೬೭೮೯ abc", source=NumeralSystem.KANNADA,
                    target=NumeralSystem.ENGLISH) == "abc 0123456789 abc"
 
 
-def test_Kannada_to_hindi1():
+def test_kannada_to_hindi1():
     assert convert("೦೧೨೩೪೫೬೭೮೯", source=NumeralSystem.KANNADA, target=NumeralSystem.HINDI) == "०१२३४५६७८९"
 
 
-def test_Kannada_to_hindi2():
+def test_kannada_to_hindi2():
     assert convert("abc ೦೧೨೩೪೫೬೭೮೯ abc", source=NumeralSystem.KANNADA,
                    target=NumeralSystem.HINDI) == "abc ०१२३४५६७८९ abc"
 
 
-def test_Kannada_to_arabic_indic1():
+def test_kannada_to_arabic_indic1():
     assert convert("೦೧೨೩೪೫೬೭೮೯", source=NumeralSystem.KANNADA, target=NumeralSystem.ARABIC_INDIC) == "٠١٢٣٤٥٦٧٨٩"
 
 
-def test_Kannada_to_arabic_indic2():
+def test_kannada_to_arabic_indic2():
     assert convert("abc ೦೧೨೩೪೫೬೭೮೯ abc", source=NumeralSystem.KANNADA,
                    target=NumeralSystem.ARABIC_INDIC) == "abc ٠١٢٣٤٥٦٧٨٩ abc"
 
 
-def test_Kannada_to_bengali1():
+def test_kannada_to_bengali1():
     assert convert("೦೧೨೩೪೫೬೭೮೯", source=NumeralSystem.KANNADA, target=NumeralSystem.BENGALI) == "০১২৩৪৫৬৭৮৯"
 
 
-def test_Kannada_to_bengali2():
+def test_kannada_to_bengali2():
     assert convert("abc ೦೧೨೩೪೫೬೭೮೯ abc", source=NumeralSystem.KANNADA,
                    target=NumeralSystem.BENGALI) == "abc ০১২৩৪৫৬৭৮৯ abc"
 
 
-def test_Kannada_to_thai1():
+def test_kannada_to_thai1():
     assert convert("೦೧೨೩೪೫೬೭೮೯", source=NumeralSystem.KANNADA, target=NumeralSystem.THAI) == "๐๑๒๓๔๕๖๗๘๙"
 
 
-def test_Kannada_to_thai2():
+def test_kannada_to_thai2():
     assert convert("abc ೦೧೨೩೪೫೬೭೮೯ abc", source=NumeralSystem.KANNADA,
                    target=NumeralSystem.THAI) == "abc ๐๑๒๓๔๕๖๗๘๙ abc"
 
 
-def test_Kannada_to_khmer1():
+def test_kannada_to_khmer1():
     assert convert("೦೧೨೩೪೫೬೭೮೯", source=NumeralSystem.KANNADA, target=NumeralSystem.KHMER) == "០១២៣៤៥៦៧៨៩"
 
 
-def test_Kannada_to_khmer2():
+def test_kannada_to_khmer2():
     assert convert("abc ೦೧೨೩೪೫೬೭೮೯ abc", source=NumeralSystem.KANNADA,
                    target=NumeralSystem.KHMER) == "abc ០១២៣៤៥៦៧៨៩ abc"
 
 
-def test_Kannada_to_burmese1():
+def test_kannada_to_burmese1():
     assert convert("೦೧೨೩೪೫೬೭೮೯", source=NumeralSystem.KANNADA, target=NumeralSystem.BURMESE) == "၀၁၂၃၄၅၆၇၈၉"
 
 
-def test_Kannada_to_burmese2():
+def test_kannada_to_burmese2():
     assert convert("abc ೦೧೨೩೪೫೬೭೮೯ abc", source=NumeralSystem.KANNADA,
                    target=NumeralSystem.BURMESE) == "abc ၀၁၂၃၄၅၆၇၈၉ abc"
 
 
-def test_Kannada_to_persian1():
+def test_kannada_to_persian1():
     assert convert("೦೧೨೩೪೫೬೭೮೯", source=NumeralSystem.KANNADA, target=NumeralSystem.PERSIAN) == "۰۱۲۳۴۵۶۷۸۹"
 
 
-def test_Kannada_to_persian2():
+def test_kannada_to_persian2():
     assert convert("abc ೦೧೨೩೪೫೬೭೮೯ abc", source=NumeralSystem.KANNADA,
                    target=NumeralSystem.PERSIAN) == "abc ۰۱۲۳۴۵۶۷۸۹ abc"
 
 
-def test_Kannada_to_gujarati1():
+def test_kannada_to_gujarati1():
     assert convert("೦೧೨೩೪೫೬೭೮೯", source=NumeralSystem.KANNADA, target=NumeralSystem.GUJARATI) == "૦૧૨૩૪૫૬૭૮૯"
 
 
-def test_Kannada_to_gujarati2():
+def test_kannada_to_gujarati2():
     assert convert("abc ೦೧೨೩೪೫೬೭೮೯ abc", source=NumeralSystem.KANNADA,
                    target=NumeralSystem.GUJARATI) == "abc ૦૧૨૩૪૫૬૭૮૯ abc"
 
 
-def test_Kannada_to_telugu1():
+def test_kannada_to_telugu1():
     assert convert("೦೧೨೩೪೫೬೭೮೯", source=NumeralSystem.KANNADA, target=NumeralSystem.TELUGU) == "౦౧౨౩౪౫౬౭౮౯"
 
 
-def test_Kannada_to_telugu2():
+def test_kannada_to_telugu2():
     assert convert("abc ೦೧೨೩೪೫೬೭೮೯ abc", source=NumeralSystem.KANNADA,
                    target=NumeralSystem.TELUGU) == "abc ౦౧౨౩౪౫౬౭౮౯ abc"
 
 
-def test_Kannada_to_odia1():
+def test_kannada_to_odia1():
     assert convert("೦೧೨೩೪೫೬೭೮೯", source=NumeralSystem.KANNADA, target=NumeralSystem.ODIA) == "୦୧୨୩୪୫୬୭୮୯"
 
 
-def test_Kannada_to_odia2():
+def test_kannada_to_odia2():
     assert convert("abc ೦೧೨೩೪೫೬೭೮೯ abc", source=NumeralSystem.KANNADA,
                    target=NumeralSystem.ODIA) == "abc ୦୧୨୩୪୫୬୭୮୯ abc"
 
 
-def test_Kannada_to_tibetan1():
+def test_kannada_to_tibetan1():
     assert convert("೦೧೨೩೪೫೬೭೮೯", source=NumeralSystem.KANNADA, target=NumeralSystem.TIBETAN) == "༠༡༢༣༤༥༦༧༨༩"
 
 
-def test_Kannada_to_tibetan2():
+def test_kannada_to_tibetan2():
     assert convert("abc ೦೧೨೩೪೫೬೭೮೯ abc", source=NumeralSystem.KANNADA,
                    target=NumeralSystem.TIBETAN) == "abc ༠༡༢༣༤༥༦༧༨༩ abc"
