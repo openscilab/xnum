12c6ead1d2c743bc4e402886ce4f5837e8b813ca
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-09-16 00:56:22 +0330

    fix : autopep8

--- tests/test_arabic_indic.py
+++ tests/test_arabic_indic.py
@@ -45,7 +45,7 @@ def test_arabic_indic_to_other_systems(target, expected):
         target=target,
     ) == expected
 
-    assert list(map(int, expected)) == [0,1,2,3,4,5,6,7,8,9]
+    assert list(map(int, expected)) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     assert convert(
         f"abc {ARABIC_INDIC_DIGITS} abc",
