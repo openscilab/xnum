f0161e007af5a4b159dfe1d6f511f624b5f6bfea
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-10-03 12:14:55 -0700

    update : tests updated.

--- tests/test_lao.py
+++ tests/test_lao.py
@@ -47,6 +47,7 @@ CONVERSION_CASES = {
     NumeralSystem.MODI: "𑙐𑙑𑙒𑙓𑙔𑙕𑙖𑙗𑙘𑙙",
     NumeralSystem.TAKRI: "𑛀𑛁𑛂𑛃𑛄𑛅𑛆𑛇𑛈𑛉",
     NumeralSystem.NEWA: "𑑐𑑑𑑒𑑓𑑔𑑕𑑖𑑗𑑘𑑙",
+    NumeralSystem.TIRHUTA: "𑓐𑓑𑓒𑓓𑓔𑓕𑓖𑓗𑓘𑓙",
 }
 
 
